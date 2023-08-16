989d375b27ba1e23726fc05a84e41dda60f07b27
JOOYOUNG KIM <jky0420@gmail.com>
2023-08-15 23:37:18 -0700

    add item button implemented

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-const greeting = 'hello world';
+const itemForm = document.getElementById('item-form');
+const itemInput = document.getElementById('item-input');
+const itemList = document.getElementById('item-list');
+
+
+// Event Handle function
+function addItem(e) {
+  e.preventDefault();
+  
+  const button = createButton("remove-item btn-link text-red");
+  const li = document.createElement('li');
+
+  const item = document.createTextNode(itemInput.value);
+  li.appendChild(item);
+  li.appendChild(button);
+  itemList.appendChild(li);
+
+  itemInput.value = '';
+}
+
+// function that create button
+function createButton(classes) {
+  const button = document.createElement('button');
+  button.className = classes;
+  const icon = createIcon("fa-solid fa-xmark");
+  button.appendChild(icon);
+  return button;
+  
+}
+
+// function that create icon
+function createIcon(classes) {
+  const icon = document.createElement('i');
+  icon.className = classes;
+  return icon;
+}
+
+
+// Event Listeners
+itemForm.addEventListener('submit', addItem);
